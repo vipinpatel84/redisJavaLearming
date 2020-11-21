816520636b51162b0c53e48c85e8be20f206b689
Vipin Patel <vipin.vipinpatel84@gmail.com>
2020-11-21 20:20:04 +0530

    Redis

--- src/main/java/com/redis/redis/RedisApplication.java
+++ src/main/java/com/redis/redis/RedisApplication.java
@@ -1,13 +1,17 @@
 package com.redis.redis;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @SpringBootApplication
 public class RedisApplication implements CommandLineRunner{
@@ -34,6 +38,16 @@ public void run(String... args) throws Exception {
 	HashMap<String,String> hashmap =(HashMap<String, String>) jedis.hgetAll("gc:user:profile");
 	
 	System.out.println(hashmap);
+	
+	jedis.sadd("planet", "pluto");
+//	Set<String> Dplanet= new HashSet<String>();
+//	Dplanet.add("mars");
+	jedis.sadd("Dplanet", "mars");
+	
+	System.out.println(jedis.smove("planet", "Dplanet", "pluto"));
+	System.out.println("Card " +jedis.scard("Dplanet"));
+	
+	
 	jedis.close();
 	String name = jedis.hget("gc:user:profile", "Name");
 	System.out.println("name " + name);
@@ -60,5 +74,26 @@ public void run(String... args) throws Exception {
 	System.out.println(jedis.scard("familyMem"));
 	System.out.println(jedis2.smembers("familyMem"));
 	jedis2.close();
+	
+//	Set<String> planet= new HashSet<String>();
+//	planet.add("pluto");
+	JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "localhost", 6379);
+
+	
+    
+	for (int i=0; i < 10; i++) {
+	   try(Jedis jedis1= jedisPool.getResource()){
+	   jedis1.set(String.valueOf(i), "0");
+	   System.out.println(jedis1.get(String.valueOf(i)));
+	}
+	}
+	
+	try (Jedis jedis3 = jedisPool.getResource()) {
+	    Long setSize = jedis3.scard("messages");
+	    System.out.println(setSize);
+	    System.out.println("Size: " + String.valueOf(setSize));
+	    jedis.srem("messages");
+	    //jedis3.close();
+	}
 }
 }
